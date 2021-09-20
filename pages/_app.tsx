ed07c00ab8c2c36f16359d455883a489e36eb5d9
Sammy Fattah <sammy.fattah@zonal.co.uk>
2021-09-20 10:41:00 +0100

    Initial project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+import '@fontsource/quicksand';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        color: '#ffffff',
+        bg: '#181818'
+      }
+    }
+  },
+  fonts: {
+    heading: 'Quicksand',
+    body: 'Quicksand'
+  },
+  colors: {
+    bg: '#181818',
+    primary: '#FE6220'
+  }
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default MyApp;
