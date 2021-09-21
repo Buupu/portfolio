f2cebe586ed79a8b22fae67eeb037ec5bd34ad3b
Buupu <sammyfattah@gmail.com>
2021-09-21 16:38:00 +0100

    Added favicon and page title

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -24,44 +24,36 @@ export default function ProjectCard({
   technologies: string[];
 }) {
   return (
-    <Link
-      href={demoLink}
-      target="_blank"
-      _hover={{ textDecoration: "none" }}
-      _active={{ outline: "none" }}
+    <Flex
+      w="full"
+      h="full"
+      p={6}
+      border="1px"
+      borderColor="lightGray"
+      borderRadius="10px"
+      flexDirection="column"
+      minH="260px"
     >
-      <Flex
-        w="full"
-        h="full"
-        p={6}
-        border="1px"
-        borderColor="lightGray"
-        borderRadius="10px"
-        flexDirection="column"
-        cursor="pointer"
-        minH="260px"
-        _hover={{ borderColor: ["lightGray", "lightGray", "primary"] }}
-        transition="0.3s ease-out"
-      >
-        <Flex justify="space-between" mb={4}>
-          <Heading as="h3" fontSize="lg" textAlign="left">
-            {title}
-          </Heading>
-          <Link href={githubLink} target="_blank">
-            <Icon
-              as={FaGithub}
-              h="24px"
-              w="24px"
-              color="whiteAlpha.600"
-              _hover={{ color: "primary" }}
-              transition="0.3s ease-out"
-            />
-          </Link>
-        </Flex>
-        <Flex flexDirection="column" flex={1} w="100%" textAlign="right">
-          <Text flex={1} fontSize={["12px", "14px"]} textAlign="left" mb={2}>
-            {description}
-          </Text>
+      <Flex justify="space-between" mb={4}>
+        <Heading as="h3" fontSize="lg" textAlign="left">
+          {title}
+        </Heading>
+        <Link href={githubLink} target="_blank">
+          <Icon
+            as={FaGithub}
+            h="24px"
+            w="24px"
+            color="whiteAlpha.600"
+            _hover={{ color: "primary" }}
+            transition="0.3s ease-out"
+          />
+        </Link>
+      </Flex>
+      <Flex flexDirection="column" flex={1} w="100%" textAlign="right">
+        <Text flex={1} fontSize={["12px", "14px"]} textAlign="left" mb={2}>
+          {description}
+        </Text>
+        <Link href={demoLink} target="_blank">
           <Text
             fontSize={["12px", "14px"]}
             fontWeight="bold"
@@ -70,25 +62,25 @@ export default function ProjectCard({
           >
             View Demo {">"}
           </Text>
-        </Flex>
-        <HStack justify="flex-end" mt={3} maxW="100%" flexWrap="wrap">
-          {technologies.map((tech) => (
-            <Box pt={1} key={`project-card-badge-${title}-${tech}`}>
-              <Badge
-                color="primary"
-                textTransform="none"
-                variant="outline"
-                boxShadow="none"
-                border="1px"
-                borderRadius="5px"
-                fontWeight="normal"
-              >
-                {tech}
-              </Badge>
-            </Box>
-          ))}
-        </HStack>
+        </Link>
       </Flex>
-    </Link>
+      <HStack justify="flex-end" mt={3} maxW="100%" flexWrap="wrap">
+        {technologies.map((tech) => (
+          <Box pt={1} key={`project-card-badge-${title}-${tech}`}>
+            <Badge
+              color="primary"
+              textTransform="none"
+              variant="outline"
+              boxShadow="none"
+              border="1px"
+              borderRadius="5px"
+              fontWeight="normal"
+            >
+              {tech}
+            </Badge>
+          </Box>
+        ))}
+      </HStack>
+    </Flex>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Head from "next/head";
 import Header from "sections/Header";
 import Intro from "sections/Intro";
 import Experience from "sections/Experience";
@@ -10,6 +11,34 @@ import Education from "sections/Education";
 const Home: NextPage = () => {
   return (
     <Box maxW="1200px" margin="auto" px={8}>
+      <Head>
+        <title>Sammy Fattah - Front-End Developer</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/images/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Header />
       <Intro />
       <Experience />
